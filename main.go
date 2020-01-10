815970c1e1f9551ca9009e80cc7dfdf03fb1baa5
Michel Wohlert <michel@1012consulting.io>
2020-01-09 23:01:33 -0800

    s

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 )
 
 func main() {
-	log.Println("TESTf1!dddfcd")
+	log.Println("TESTf1!dddfcdd")
 	err := ioutil.WriteFile("/data/testFile", []byte("Hello World"), 0644)
 	if err != nil {
 		log.Println("Error writing testfile")
